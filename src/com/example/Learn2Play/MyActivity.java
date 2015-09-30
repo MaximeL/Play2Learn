af1a725f5fa3e1c6dfc2873515376ed1809162d2
MarcDestefanis <marc.destefanis.etu@gmail.com>
2015-09-30 23:10:25 +0200

    switch activities, transition fade-in fade-out

--- src/com/example/Learn2Play/MyActivity.java
+++ src/com/example/Learn2Play/MyActivity.java
@@ -1,9 +1,15 @@
 package com.example.Learn2Play;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
+    private TextView jouer;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +17,17 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        jouer = (Button) findViewById(R.id.jouer);
+
+        jouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MyActivity.this, ModeColors.class);
+                startActivity(intent);
+                overridePendingTransition(R.transition.fade_in_opacity, R.transition.fade_out_opacity);
+                finish();
+            }
+        });
     }
 }
