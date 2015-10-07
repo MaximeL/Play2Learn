fada70a5ae69bb39ef37e6bfb394d2a37686cd72
Marc Destefanis <marc.destefanis.etu@gmail.com>
2015-10-07 22:50:31 +0200

    bug transition activities fixed

--- src/com/example/Learn2Play/MyActivity.java
+++ src/com/example/Learn2Play/MyActivity.java
@@ -5,27 +5,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
-    private TextView jouer;
+    private ImageButton buttonJouer;
 
     /**
      * Called when the activity is first created.
      */
-    //@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        jouer = (Button) findViewById(R.id.jouer);
+        buttonJouer = (ImageButton) findViewById(R.id.buttonJouer);
 
-        jouer.setOnClickListener(new View.OnClickListener() {
+        buttonJouer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MyActivity.this, ModeColors.class);
+                Intent intent = new Intent(MyActivity.this, Niveau1.class);
                 startActivity(intent);
-                //overridePendingTransition(R.transition.fade_in_opacity, R.transition.fade_out_opacity);
+                overridePendingTransition(R.transition.fade_in_opacity, R.transition.fade_out_opacity);
                 finish();
             }
         });
