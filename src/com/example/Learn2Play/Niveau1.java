2f9e9ecad0347ccd3a695fd7275a46c1b1dd7a72
Marc Destefanis <marc.destefanis.etu@gmail.com>
2015-10-14 09:57:56 +0200

    LinearLayout niveau pour le oui et le non

--- src/com/example/Learn2Play/Niveau1.java
+++ src/com/example/Learn2Play/Niveau1.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Collections;
